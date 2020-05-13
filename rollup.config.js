12b2f4786a5263311664a917187e8886d5c1cdb5
Felipe Lujan <34170261+FelipeLujan@users.noreply.github.com>
2020-05-12 20:38:47 -0500

    tried some changes, still no success

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,12 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
+import { uglify } from 'rollup-plugin-uglify';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJSON from './package.json';
+
+const minifyExtension = (pathToFile) => pathToFile.replace(/\.js$/, '.min.js');
 
 export default [
 	// CommonJS
@@ -14,12 +18,30 @@ export default [
 			format: 'cjs',
 		},
 		plugins: [
+			peerDepsExternal(),
 			babel({
 				exclude: 'node_modules/**',
 			}),
-			external(),
+			external(['react', 'react-dom', 'styled-components', 'react-spring']),
 			resolve(),
 			commonjs(),
+		],
+	},
+	{
+		input: './src/index.js',
+		output: {
+			file: minifyExtension(packageJSON.main),
+			format: 'cjs',
+		},
+		plugins: [
+			peerDepsExternal(),
+			babel({
+				exclude: 'node_modules/**',
+			}),
+			external(['react', 'react-dom', 'styled-components', 'react-spring']),
+			resolve(),
+			commonjs(),
+			uglify(),
 		],
 	},
 ];
